eb559214142805687e5ad6ab4fc4aeff92f342e5
JeffreyRock <43006938+JeffreyRock@users.noreply.github.com>
2024-06-25 14:53:05 -0600

    C++ engine build
    Fixed Build Script

--- C++Build.groovy
+++ C++Build.groovy
@@ -1,47 +1,56 @@
 pipeline {
     agent any
 
-    enviroment {
-        Build_Dir ='build'
-        MSBuild_Path "C:\\"
-        Warning = False
-        Failed = False
+    environment {
+        Build_Dir = 'build'
+        MSBuild_Path = "C:\\"
+        Warning = false
+        Failed = false
     }
-    parameters {
-        
-    }
-    stages{
-        stage ("Git Checkout"){
-            Git 
-        }
-        stage("Build Bash"){
-            when{
-                expression(env.NODE_LABELS.contains('Bash'))
-            }
-            steps{
 
+    stages {
+        stage("Git Checkout") {
+            steps {
+                git credentialsId: 'GitUserAccount', url: 'https://github.com/JeffreyRock/OpenGLEngine'
             }
         }
-        Stage ("Build WIndows "){
-            when{
-                expression(env.NODE_LABELS.contains('Windows'))
+        stage("Build Bash") {
+            when {
+                expression { env.NODE_LABELS.contains('Bash') }
             }
-            steps{
-                bat """
-                    mkdir %Build_Dir%
-                    CMAKE --build -S . -B %Build_Dir%
+            steps {
+                sh """
+                    mkdir -p $Build_Dir
+                    cmake --build -S . -B $Build_Dir
                 """
             }
         }
-        Stage ("Test")
-        {
-
+        stage("Build Windows") {
+            when {
+                expression { env.NODE_LABELS.contains('Windows') }
+            }
+            steps {
+                bat """
+                    mkdir %Build_Dir%
+                    g++ -I .\Engine\ConsoleApplication1\ConsoleApplication1\GLFW\glfw-3.4.bin.WIN64\include -I .\Engine\ConsoleApplication1\ConsoleApplication1\GLFW\glfw-3.4.bin.WIN64\include\glad -L .\Engine\ConsoleApplication1\ConsoleApplication1\GLFW\glfw-3.4.bin.WIN64\lib-mingw-w64 .\Engine\ConsoleApplication1\ConsoleApplication1\ConsoleApplication1.cpp .\Engine\ConsoleApplication1\ConsoleApplication1\GLFW\glfw-3.4.bin.WIN64\include\glad\glad.c -lglfw3 -lopengl32 -lgdi32 -o .\build\build.exe
+                """
+            }
         }
-        stage ("Archive"){
-
+        stage("Test") {
+            steps {
+                echo "Test Complete"
+            }
         }
-        post{
-            
+        stage("Archive") {
+            steps {
+                echo "Archive Complete"
+            }
+        }
+    }
+
+    post {
+        always {
+            echo "Pipeline execution finished"
         }
     }
 }
